4016ef0f80ab6a60166049ec7826c771a54cc0bc
rainingyard <251606695@qq.com>
2017-06-15 16:20:41 +0800

    quicksort --c++

--- sort/quicksort.cpp
+++ sort/quicksort.cpp
@@ -2,6 +2,14 @@
 #include <vector>
 using namespace std;
 
+/*
+实现思路是这样的：
+1.当只有一个元素时，它总是已经排好序的直接返回。
+2.取最后一个为pivot，比pivot小的元素存储在[0,lessend)[0,lessend)中。
+3.遍历[begin,end−1)[begin,end−1)，如果它小于pivot就把它添加到[0,lessend)[0,lessend)中，同时让less_end++。
+4.将pivot放到[0,lessend)[0,lessend)的结尾。
+5.为[begin,lessend)[begin,lessend)排序，此时less_end处的元素是pivot；同样为右边的[lessend+1,end)[lessend+1,end)也排序。
+*/
 void quicksort(vector<int>& v, int begin, int end){
     if(end - begin<=1) return;
     int pivot = v[end-1], less_end = begin;
